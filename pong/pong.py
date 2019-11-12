178b7c872603ebc7ef95a937b6f26098b3b31e22
Janson <jansonchiu343@gmail.com>
2019-11-11 17:57:36 -0800

    add paddle a and b

--- pong/pong.py
+++ pong/pong.py
@@ -8,7 +8,29 @@ win.setup(width=800, height=600)
 # This stops the window from updating 
 win.tracer(0)
 
+# Paddle A 
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+# This stretches the width by 5 times 
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+#This starts the paddle on the left
+paddle_a.goto(-350, 0)
+
+# Paddle B 
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+# This stretches the width by 5 times 
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+#This starts the paddle on the right
+paddle_b.goto(350, 0)
+
+
 # Main game loop 
 while True: 
-  wn.update()
-  
+  win.update()
