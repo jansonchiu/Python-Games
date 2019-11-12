66158d10ea84e6844e10018e0bef1baaab28ba02
Janson <jansonchiu343@gmail.com>
2019-11-11 17:47:54 -0800

    add pong.py file, set sizing

---
+++ pong/pong.py
@@ -0,0 +1,14 @@
+import turtle
+
+win = turtle.Screen()
+win.title("Pong by @jansonchiu")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+ 
+# This stops the window from updating 
+win.tracer(0)
+
+# Main game loop 
+while True: 
+  wn.update()
+  
