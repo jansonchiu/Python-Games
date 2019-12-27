40591d236aab0e6f5da5678d741353dd14f670d9
Janson <jansonchiu343@gmail.com>
2019-12-26 20:14:22 -0800

    almost done drawing the board, need to import pygame module

--- snake/snake.py
+++ snake/snake.py
@@ -27,27 +27,44 @@ class snake(object):
     pass
     
 def drawGrid(w, rows, surface):
-  pass
-def redrawWindow(surface):
-  pass
-def randomSnack(rows, items):
+  sizeBtwn = w // rows 
+
+  x = 0 
+  y = 0 
+  for l in range(rows):
+      x = x+ sizeBtwn
+      y = y + sizeBtwn
+
+      pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+      pygame.draw.line(surface, (255, 255, 255), (0,y), (w, y))
   pass
 
+def redrawWindow(surface):
+  global rows, width
+  surface.fill((0,0,0))
+  drawGrid(width, rows, surface)
+  pygame.display.update()
+  
 def randomSnack(rows, items):
   pass
 
 def message_box(subject, content):
   pass
 
-def main() : 
-  win = pygame.display.set_mode()
+def main(): 
+  global width, rows
+  width = 500 
+  rows = 20
+  win = pygame.display.set_mode((width, width))
+  s = snake((255, 0, 0), (10,10))
+  flag = True
+
+  clock = pygame.time.Clock()
+
+  while flag: 
+      pygame.time.delay(50)
+      clock.tick(10)
+      redrawWindow(win)
   pass
 
-rows = 
-w = 
-h = 
-
-
-cube.rows = rows
-cube.w = w
 main()
